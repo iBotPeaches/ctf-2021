22c669cfc50c1bb33ff77b74816da29e01393aa6
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-09-30 06:09:44 -0400

    feat: challenge 22 (web)

---
+++ web/challenges/22/index.php
@@ -0,0 +1,34 @@
+<?php
+
+header('X-Cat-Dog: TOAD{s0_h77P_h34D3rs-r-K3Wl}');
+
+echo <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../common/bootstrap.min.css"/>
+    <title>Challenge 22</title>
+</head>
+<script>
+</script>
+<body>
+<div class="container">
+    <h2>The Sourcetoad CTF (2021)</h2>
+    <div class="text-center">
+        <span class="label label-default">The</span>
+        <span class="label label-primary">flag</span>
+        <span class="label label-success">was</span>
+        <span class="label label-default">delivered</span>
+        <span class="label label-warning">to</span>
+        <span class="label label-default">this</span>
+        <span class="label label-info">page.</span>
+        <span class="label label-default">Can</span>
+        <span class="label label-danger">you</span>
+        <span class="label label-default">see</span>
+        <span class="label label-default">it?</span>
+    </div>
+</div>
+</body>
+</html>
+HTML;
