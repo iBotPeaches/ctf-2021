dd7c1d33fce432e2631c935aff9e904d98bb921c
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-10-02 11:38:49 -0400

    feat: challenge 25 + reorganize

---
+++ setup/23/README.md
@@ -0,0 +1,3 @@
+# Setup Challenge 23
+
+1. Place `index.html` into default location for nginx/apache to serve
---
+++ setup/25/laravel/resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bootstrap.min.css"/>
+    <title>Challenge 25</title>
+</head>
+<body>
+<div class="container">
+    <h2>The Sourcetoad CTF (2021)</h2>
+    <p>
+        Finally! We completed our upgrade to Laravel 8!!
+    </p>
+    <p>
+        We took this chance to abandon Whoops! (Thanks for the years of error tracking)
+    </p>
+    <p>
+        We have now installed Ignition 2.5.1, which should help us with any issues we encounter
+    </p>
+    <p>
+        We are trying to build a CTF, but we haven't yet hide the flag in a reachable area. Its still
+        in the root filesystem at <kbd>/flag</kbd>.
+    </p>
+    <a href="/dashboard">You can peek around in the meantime.</a>
+</div>
+</body>
+</html>
