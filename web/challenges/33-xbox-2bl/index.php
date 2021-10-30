a18c15a7dfb163dd2aa8b2233b241d411c169cb5
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-10-30 07:15:13 -0400

    feat: challenge 33

---
+++ web/challenges/33-xbox-2bl/index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../common/bootstrap.min.css"/>
+    <title>Challenge 33 (Xbox 2BL)</title>
+</head>
+<body>
+<div class="container">
+    <h2>The Sourcetoad CTF (2021)</h2>
+    <div class="text-center">
+        <p>
+            The key you seek is ____@c64.org
+        </p><!-- 4 letters -->
+        <pre>08 dd 93 74 36 86 17 82 04 f9 8a 90 12 87 eb 36 fd f7 88 08 f2 b5 a3 88 7a</pre>
+    </div>
+</div>
+</body>
+</html>
+HTML;
